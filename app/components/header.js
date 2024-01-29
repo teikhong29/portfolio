c3987dd9b52577f6364f6875f315f5b07c69ed63
Jackie Tan <156156270+teikhong29@users.noreply.github.com>
2024-01-29 09:34:38 +0800

    added favicon

--- app/components/header.js
+++ app/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
             <nav id="navbar" className="navbar nav-menu">
                 <ul>
                     <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></a></li>
-                    <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></a></li>
+                    <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About Me</span></a></li>
                     <li><a href="#eduCert" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Education & Certification</span></a></li>
                     <li><a href="#technicalSkill" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Technical Skills</span></a></li>
                 </ul>
